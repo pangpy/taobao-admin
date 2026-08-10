3ecfc82b255ce5e4b8ef97bab0443641ab07fba9
pangpy <47621088+pangpy@users.noreply.github.com>
2026-08-10 16:37:01 +0800

    新增ws salt

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -5,6 +5,7 @@ interface ImportMetaEnv {
   readonly VITE_API_ACCOUNT: string
   readonly VITE_TOTP_SECRET: string
   readonly VITE_ACCESS_TOKEN: string
+  readonly VITE_WS_SALT: string  // ← 新增
 }
 
 interface ImportMeta {
